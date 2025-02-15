30c1f0140326d86cba0344ca6e3c9e6ff3822a2b
Rain Jiang <rain-jiang@outlook.com>
2025-02-14 16:18:38 -0800

    refactor code and add back the flash attn splitwise

--- vllm/distributed/kv_transfer/kv_connector/simple_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/simple_connector.py
@@ -19,6 +19,8 @@ from vllm.distributed.kv_transfer.kv_lookup_buffer.simple_buffer import (
     SimpleBuffer)
 from vllm.logger import init_logger
 from vllm.sequence import IntermediateTensors
+from vllm.attention.backends.flash_attn import FlashAttentionMetadata
+from vllm.attention.backends.triton_mla import TritonMLAMetadata
 
 if TYPE_CHECKING:
     from vllm.worker.model_runner import ModelInputForGPUWithSamplingMetadata
@@ -171,48 +173,57 @@ class SimpleConnector(KVConnectorBase):
         # query_lens contains new KV caches that are added to vLLM.
         # so we will send them to decode instance
         # FIXME(Kuntai): This assume that all requests are prefill.
-        for idx, slen in enumerate(seq_lens):
-            start_pos = sum(seq_lens[:idx])
-            end_pos = start_pos + slen
-            current_tokens = input_tokens_tensor[start_pos:end_pos]
+        # flash attn
+        if isinstance(model_input.attn_metadata, FlashAttentionMetadata):
+            for idx, slen in enumerate(seq_lens):
+                start_pos = sum(seq_lens[:idx])
+                end_pos = start_pos + slen
+                current_tokens = input_tokens_tensor[start_pos:end_pos]
 
-            keys, values = [], []
+                keys, values = [], []
 
-            # flash attn
-            # for layer_id in range(start_layer, end_layer):
-            #     kv_cache = kv_caches[layer_id - start_layer]
+                for layer_id in range(start_layer, end_layer):
+                    kv_cache = kv_caches[layer_id - start_layer]
 
-            #     key_cache = kv_cache[0].reshape(-1, num_heads, head_size)
-            #     value_cache = kv_cache[1].reshape(-1, num_heads, head_size)
+                    key_cache = kv_cache[0].reshape(-1, num_heads, head_size)
+                    value_cache = kv_cache[1].reshape(-1, num_heads, head_size)
 
-            #     current_slot_mapping = slot_mapping_flat[start_pos:end_pos]
+                    current_slot_mapping = slot_mapping_flat[start_pos:end_pos]
 
-            #     keys.append(key_cache[current_slot_mapping].unsqueeze(0))
-            #     values.append(value_cache[current_slot_mapping].unsqueeze(0))
+                    keys.append(key_cache[current_slot_mapping].unsqueeze(0))
+                    values.append(value_cache[current_slot_mapping].unsqueeze(0))
 
-            # keys = torch.cat(keys, dim=0)
-            # values = torch.cat(values, dim=0)
-            
-            # mla
-            for layer_id in range(start_layer, end_layer):
-                key_cache = kv_caches[layer_id - start_layer]
+                keys = torch.cat(keys, dim=0)
+                values = torch.cat(values, dim=0)
 
-                # key_cache = kv_cache.reshape(-1, num_heads, head_size)
-                # value_cache = kv_cache[1].reshape(-1, num_heads, head_size)
-
-                current_slot_mapping = slot_mapping_flat[start_pos:end_pos]
-
-                keys.append(key_cache[current_slot_mapping].unsqueeze(0))
-                # values.append(value_cache[current_slot_mapping].unsqueeze(0))
-
-            keys = torch.cat(keys, dim=0)
-            # values = torch.cat(values, dim=0)
-            values = torch.zeros(1)
-
-            self.insert(current_tokens,
+                self.insert(current_tokens,
                         torch.ones_like(current_tokens,
                                         dtype=bool), keys, values,
                         hidden_or_intermediate_states[start_pos:end_pos])
+
+        # mla
+        elif isinstance(model_input.attn_metadata, TritonMLAMetadata):
+            for idx, slen in enumerate(seq_lens):
+                start_pos = sum(seq_lens[:idx])
+                end_pos = start_pos + slen
+                current_tokens = input_tokens_tensor[start_pos:end_pos]
+
+                keys, values = [], []
+
+                for layer_id in range(start_layer, end_layer):
+                    kv_cache = kv_caches[layer_id - start_layer]
+
+                    current_slot_mapping = slot_mapping_flat[start_pos:end_pos]
+
+                    keys.append(kv_cache[current_slot_mapping].unsqueeze(0))
+
+                keys = torch.cat(keys, dim=0)
+                values = torch.zeros(1) # dummy value
+
+                self.insert(current_tokens,
+                            torch.ones_like(current_tokens,
+                                            dtype=bool), keys, values,
+                            hidden_or_intermediate_states[start_pos:end_pos])
 
         logger.debug("[rank%d]: KV send DONE.", torch.distributed.get_rank())
 
@@ -241,77 +252,118 @@ class SimpleConnector(KVConnectorBase):
 
         # enumerate different requests
         # FIXME(Kuntai): This impl assumes that all requests are prefill.
-        for idx, slen in enumerate(seq_lens):
+        # flash attn
+        if isinstance(model_input.attn_metadata, FlashAttentionMetadata):
+            for idx, slen in enumerate(seq_lens):
 
-            start_pos = sum(seq_lens[:idx])
-            end_pos = start_pos + slen
-            current_tokens = input_tokens_tensor[start_pos:end_pos]
-            num_tokens = slen
+                start_pos = sum(seq_lens[:idx])
+                end_pos = start_pos + slen
+                current_tokens = input_tokens_tensor[start_pos:end_pos]
+                num_tokens = slen
 
-            # collecting data for rebuilding the input
-            input_tokens_list.append(current_tokens)
-            start_pos_list.append(start_pos)
+                # collecting data for rebuilding the input
+                input_tokens_list.append(current_tokens)
+                start_pos_list.append(start_pos)
 
-            ret = self.select(current_tokens,
-                              torch.ones_like(current_tokens, dtype=bool))
-            if ret[0] is None:
-                # didn't find any match.
-                bypass_model_exec = False
-                num_computed_tokens_list.append(0)
-                continue
+                ret = self.select(current_tokens,
+                                torch.ones_like(current_tokens, dtype=bool))
+                if ret[0] is None:
+                    # didn't find any match.
+                    bypass_model_exec = False
+                    num_computed_tokens_list.append(0)
+                    continue
 
-            roi: torch.Tensor = ret[1]
-            keys: torch.Tensor = ret[2]
-            values: torch.Tensor = ret[3]
-            hidden: torch.Tensor = ret[4]
+                roi: torch.Tensor = ret[1]
+                keys: torch.Tensor = ret[2]
+                values: torch.Tensor = ret[3]
+                hidden: torch.Tensor = ret[4]
 
-            num_computed_tokens = roi.shape[0]
-            num_computed_tokens_list.append(num_computed_tokens)
+                num_computed_tokens = roi.shape[0]
+                num_computed_tokens_list.append(num_computed_tokens)
 
-            # check if both KV cache and the hidden states are received
-            # If not, need to redo the forwarding to compute missing states
-            if not all([(num_computed_tokens == num_tokens), hidden is not None
-                        ]):
-                bypass_model_exec = False
+                # check if both KV cache and the hidden states are received
+                # If not, need to redo the forwarding to compute missing states
+                if not all([(num_computed_tokens == num_tokens), hidden is not None
+                            ]):
+                    bypass_model_exec = False
 
-            # update the end position based on how many tokens are cached.
-            end_pos = start_pos + num_computed_tokens
+                # update the end position based on how many tokens are cached.
+                end_pos = start_pos + num_computed_tokens
 
-            # put received KV caches into paged memory
-            # for i in range(model_executable.model.start_layer,
-            #                model_executable.model.end_layer):
+                # put received KV caches into paged memory
+                for i in range(model_executable.model.start_layer,
+                               model_executable.model.end_layer):
 
-            #     kv_cache = kv_caches[i - model_executable.model.start_layer]
-            #     layer = model_executable.model.layers[i]
+                    kv_cache = kv_caches[i - model_executable.model.start_layer]
+                    layer = model_executable.model.layers[i]
 
-            #     key_cache, value_cache = kv_cache[0], kv_cache[1]
-            #     ops.reshape_and_cache_flash(
-            #         keys[i - model_executable.model.start_layer].to(
-            #             key_cache.device),
-            #         values[i - model_executable.model.start_layer].to(
-            #             value_cache.device),
-            #         key_cache,
-            #         value_cache,
-            #         slot_mapping[start_pos:end_pos],
-            #         layer.self_attn.attn.kv_cache_dtype,
-            #         layer.self_attn.attn._k_scale,
-            #         layer.self_attn.attn._v_scale,
-            #     )
+                    key_cache, value_cache = kv_cache[0], kv_cache[1]
+                    ops.reshape_and_cache_flash(
+                        keys[i - model_executable.model.start_layer].to(
+                            key_cache.device),
+                        values[i - model_executable.model.start_layer].to(
+                            value_cache.device),
+                        key_cache,
+                        value_cache,
+                        slot_mapping[start_pos:end_pos],
+                        layer.self_attn.attn.kv_cache_dtype,
+                        layer.self_attn.attn._k_scale,
+                        layer.self_attn.attn._v_scale,
+                    )
 
-            keys_list = []
-            for i in range(keys.shape[0]):
-                keys_list.append(keys[i])
+                hidden_or_intermediate_states_for_one_req.append(hidden)
+        # mla
+        elif isinstance(model_input.attn_metadata, TritonMLAMetadata):
+            for idx, slen in enumerate(seq_lens):
 
-            current_slot_mapping = slot_mapping[start_pos:end_pos]
+                start_pos = sum(seq_lens[:idx])
+                end_pos = start_pos + slen
+                current_tokens = input_tokens_tensor[start_pos:end_pos]
+                num_tokens = slen
 
-            # put received KV caches into paged memory
-            for i in range(model_executable.model.start_layer,
-                           model_executable.model.end_layer):
+                # collecting data for rebuilding the input
+                input_tokens_list.append(current_tokens)
+                start_pos_list.append(start_pos)
 
-                kv_cache = kv_caches[i - model_executable.model.start_layer]
-                kv_cache[current_slot_mapping] = keys_list[i]
+                ret = self.select(current_tokens,
+                                torch.ones_like(current_tokens, dtype=bool))
+                if ret[0] is None:
+                    # didn't find any match.
+                    bypass_model_exec = False
+                    num_computed_tokens_list.append(0)
+                    continue
 
-            hidden_or_intermediate_states_for_one_req.append(hidden)
+                roi: torch.Tensor = ret[1]
+                keys: torch.Tensor = ret[2]
+                values: torch.Tensor = ret[3] # dummy value
+                hidden: torch.Tensor = ret[4]
+
+                num_computed_tokens = roi.shape[0]
+                num_computed_tokens_list.append(num_computed_tokens)
+
+                # check if both KV cache and the hidden states are received
+                # If not, need to redo the forwarding to compute missing states
+                if not all([(num_computed_tokens == num_tokens), hidden is not None
+                            ]):
+                    bypass_model_exec = False
+
+                # update the end position based on how many tokens are cached.
+                end_pos = start_pos + num_computed_tokens
+
+                keys_list = []
+                for i in range(keys.shape[0]):
+                    keys_list.append(keys[i])
+
+                current_slot_mapping = slot_mapping[start_pos:end_pos]
+
+                # put received KV caches into paged memory
+                for i in range(model_executable.model.start_layer,
+                            model_executable.model.end_layer):
+
+                    kv_cache = kv_caches[i - model_executable.model.start_layer]
+                    kv_cache[current_slot_mapping] = keys_list[i]
+
+                hidden_or_intermediate_states_for_one_req.append(hidden)
 
         if not bypass_model_exec:
             # Some of the KV cache is not retrieved
