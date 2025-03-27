6c846e20773ae77849be6d0c09a163a8b3c9e173
xueshunyuan <xueshunyuan@bytedance.com>
2025-03-27 22:46:30 +0800

    fix for triton 3.2

--- vllm/model_executor/layers/fused_moe/fused_moe.py
+++ vllm/model_executor/layers/fused_moe/fused_moe.py
@@ -190,9 +190,10 @@ def fused_moe_kernel_gptq_awq(
                     (offs_k[None, :] < K - k * BLOCK_SIZE_K),
                     other=0.0)
         b = tl.load(
-            b_ptrs,
-            cache_modifier=".cg",
-            eviction_policy="evict_last",
+            b_ptrs
+            # b_ptrs,
+            # cache_modifier=".cg",
+            # eviction_policy="evict_last",
         )
         if use_int4_w4a16:
             b = (b >> b_shifter) & 0xF
