c1271d1a6128884a4907e80a11fbdf8e893212c9
Hank Han <hanhan7630@outlook.com>
2026-09-11 00:59:18 +0800

    Retry partial NVSHMEM downloads
    Signed-off-by: Hank Han <hanhan7630@outlook.com>
    Co-authored-by: TRAE CLI <traecli@bytedance.com>

--- tools/ep_kernels/install_python_libraries.sh
+++ tools/ep_kernels/install_python_libraries.sh
@@ -104,7 +104,9 @@ NVSHMEM_URL="https://developer.download.nvidia.com/compute/nvshmem/redist/libnvs
 
 pushd "$WORKSPACE"
 echo "Downloading NVSHMEM ${NVSHMEM_VER} for ${NVSHMEM_SUBDIR} ..."
-curl -fSL --retry 3 --retry-delay 2 "${NVSHMEM_URL}" -o "${NVSHMEM_FILE}"
+curl -fSL \
+    --retry 5 --retry-delay 5 --retry-all-errors \
+    "${NVSHMEM_URL}" -o "${NVSHMEM_FILE}"
 tar -xf "${NVSHMEM_FILE}"
 rm -rf nvshmem
 mv "${NVSHMEM_FILE%.tar.xz}" nvshmem
