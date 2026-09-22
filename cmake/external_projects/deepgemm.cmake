899326c026d16025413d99c0829d63d082e95ac6
wangyicong <wangyicong@bytedance.com>
2026-09-22 13:15:02 +0800

    build: pin buildable DeepGEMM merge

--- cmake/external_projects/deepgemm.cmake
+++ cmake/external_projects/deepgemm.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 # docker/Dockerfile.
 set(_DEEPGEMM_UPSTREAM_REPO "https://github.com/wangyicong52/DeepGEMM.git")
 set(_DEEPGEMM_UPSTREAM_COMMIT
-  "a4dee1ca421c39fc7c305615fac368c2b13aeb86")
+  "54ae30693684ae9146121e37a73823c5a3617abe")
 
 # If DEEPGEMM_SRC_DIR is set, DeepGEMM is built from that directory
 # instead of downloading.
--- tools/install_deepgemm.sh
+++ tools/install_deepgemm.sh
@@ -10,7 +10,7 @@ set -e
 DEEPGEMM_GIT_REPO="${DEEPGEMM_GIT_REPOSITORY:-https://github.com/wangyicong52/DeepGEMM.git}"
 # NOTE: This targets wyc/vllm-mega-moe, which carries the SM90 MegaMoE APIs
 # needed by DeepSeek-V4.1 Flash plus the paged-MQA indexer APIs used by vLLM.
-DEEPGEMM_GIT_REF="${DEEPGEMM_GIT_COMMIT:-a4dee1ca421c39fc7c305615fac368c2b13aeb86}"
+DEEPGEMM_GIT_REF="${DEEPGEMM_GIT_COMMIT:-54ae30693684ae9146121e37a73823c5a3617abe}"
 WHEEL_DIR=""
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
